9c42bcfbabc05534666d931508c9cdd34f3ee212
Jef <jefersontamono@gmail.com>
2024-06-16 19:05:40 +0900

    removed comment

--- js/index.js
+++ js/index.js
@@ -1,23 +1,18 @@
-// 1 passo - dar um jeito de pegar o elemento HTML da seta avancar
 const btnAvancar = document.getElementById("btn-avancar")
 const btnVoltar = document.getElementById("btn-voltar")
-// 3 passo - fazer aparecer o proximo cartao da lista
 const cartoes = document.querySelectorAll(".cartao")
 let cartaoAtual = 0
 
 
-// 2 passo - dar um jeito de identificar o clique
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return
 
-  // 4 passo - remover o selecionada no cartao anterior
   esconderCartoesSelecionado()
 
   cartaoAtual++
   mostrarCartao()
 })
 
-// vontar
 btnVoltar.addEventListener("click", function () {
   console.log("voltar clk")
   if (cartaoAtual === 0) return
