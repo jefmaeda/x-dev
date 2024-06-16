cc9f72a3d0e15705635147577f6957a2b9d84d6f
Jef <jefersontamono@gmail.com>
2024-06-16 19:01:51 +0900

    card to change complet

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
 // 1 passo - dar um jeito de pegar o elemento HTML da seta avancar
 const btnAvancar = document.getElementById("btn-avancar")
-let cartaoAtual = 0
+const btnVoltar = document.getElementById("btn-voltar")
 // 3 passo - fazer aparecer o proximo cartao da lista
 const cartoes = document.querySelectorAll(".cartao")
+let cartaoAtual = 0
+
 
 // 2 passo - dar um jeito de identificar o clique
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return
 
   // 4 passo - remover o selecionada no cartao anterior
-  const cartaoSelecionado = document.querySelector(".selecionado")
-  cartaoSelecionado.classList.remove("selecionado")
+  esconderCartoesSelecionado()
 
   cartaoAtual++
-  cartoes[cartaoAtual].classList.add("selecionado")
+  mostrarCartao()
 })
+
+// vontar
+btnVoltar.addEventListener("click", function () {
+  console.log("voltar clk")
+  if (cartaoAtual === 0) return
+
+  esconderCartoesSelecionado()
+
+  cartaoAtual--
+  mostrarCartao()
+})
+
+function mostrarCartao() {
+  cartoes[cartaoAtual].classList.add("selecionado")
+}
+
+function esconderCartoesSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado")
+  cartaoSelecionado.classList.remove("selecionado")
+}
